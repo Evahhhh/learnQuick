039f50847ebcc9d004e87220a2457b67041fa8de
Evahhhh <eve-anneoffredi@orange.fr>
2022-10-26 15:15:15 +0200

    audio player not really fixed

--- learn-quick-vue/components/FilesAudio.vue
+++ learn-quick-vue/components/FilesAudio.vue
@@ -1,11 +1,27 @@
 <template>
     <div class="content-card">
         <p>Audio</p>
+        <div class='audio-player'>
+            <AudioPlayer 
+            :option="{
+                src: '/f1-piano.mp3',
+                title: 'Piano F1',
+                coverImage: '/f1-piano-cover.png',
+            }" />
+        </div>
     </div>
 </template>
 
 <script>
+import AudioPlayer from 'vue3-audio-player'
+import 'vue3-audio-player/dist/style.css'
+
 export default {
+    name: 'FilesAudio',
+    components: {
+        name : 'AudioPlayer',
+        AudioPlayer,
+    },
 }
 </script>
 
--- learn-quick-vue/components/FilesVideo.vue
+++ learn-quick-vue/components/FilesVideo.vue
@@ -1,18 +1,33 @@
 <template>
-    <div class="content-card">
+    <div class="content-card-video">
         <h1>Vidéo</h1>
+        <div class="video-player">
+            <LazyYoutube src="https://youtu.be/dQw4w9WgXcQ" aspect-ratio="16:9" thumbnail-quality="standard" />
+        </div>
     </div>
 </template>
 
 <script>
+import {LazyYoutube} from 'vue-lazytube'
+
 export default {
+    name: 'FilesVideo',
+    components: {
+        LazyYoutube,
+    },
 }
 </script>
 
 <style>
-.content-card{
+.content-card-video{
     background : white;
     text-align  : center;
     margin-top : 30px;
+}
+
+.video-player{
+    margin-top : 30px;
+    margin-left : 10px;
+    margin-right : 10px;
 }
 </style>
